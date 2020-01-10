9253ffb28cea078bf6fe39e4d319b379ed36b3bf
tharinduvindula <tharinduvindula@gmail.com>
2020-01-10 22:31:04 +0530

    host2

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors({ origin: "*" }));
 app.use(bodyParser.json());
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("The server started on port 3000 !!!!!!");
 });
 
